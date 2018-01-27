cbdd7a546e42ded5944b3611f98f79bc7352355e
yobe <smail21yoshi@gmail.com>
2018-01-27 16:04:07 +0900

    get event & search list

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,LoadingController } from 'ionic-angular';
+import { EventProvider } from '../../providers/event/event';
 
 /**
  * Generated class for the SearchPage page.
@@ -15,8 +16,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SearchPage {
   keywords: string = "";
+  events: any[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    public eventProvider: EventProvider) {
   }
 
   ionViewDidLoad() {
@@ -24,6 +29,25 @@ export class SearchPage {
   }
 
   getEvents(ev) {
-    console.log(this.keywords);
+    const searchKeywords:string = this.keywords.trim();
+
+    if (!searchKeywords) return;
+
+    const loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    loader.present();
+
+    const kwds = searchKeywords.split(' ').filter(v => v !== "");
+    this.eventProvider.search(kwds).subscribe((body: any) => {
+      if (body && body.events) {
+        if (this.keywords === searchKeywords) {
+          this.events = body.events;
+        }
+      }
+      loader.dismiss();
+    }, (error: any) => {
+      loader.dismiss();
+    })
   }
 }
